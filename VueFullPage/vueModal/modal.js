569cd4d02c989863ceb66bd08bd316c6efbf966e
AlenKljuno <80717857+AlenKljuno@users.noreply.github.com>
2021-03-29 01:40:10 +0200

    Add files via upload

---
+++ VueFullPage/vueModal/modal.js
@@ -0,0 +1,34 @@
+new Vue({
+    el: '#app',
+    data () {
+      return {
+        showModal : false,
+        showModal2 : false,
+        showModal3 : false,
+        showModal4 : false,
+        showModal5 : false,
+        cards : [
+          {imgSrc:'https://images.vexels.com/media/users/3/142825/isolated/preview/19d912e3c1fd21e43553a6a953cfbb98-stroke-blue-jean-clothing-by-vexels.png',title:'Jeans'}
+        ],
+        cards2 : [
+          {imgSrc:'https://images.vexels.com/media/users/3/156547/isolated/preview/3c412d5ece1aa73c6a2be5be748e0cfa-basketball-shoe-icon-by-vexels.png',title:'Shoes'}
+        ],
+        cards3 : [
+          {imgSrc:'https://freesvg.org/img/DigitaLink_Blank_T-Shirt.png',title:'Shirt'}
+        ],
+        cards4 : [
+          {imgSrc:'https://images.vexels.com/media/users/3/140667/isolated/preview/88abb6d7d7794a479e93d20290654ac2-ladies-hand-bag-4-by-vexels.png',title:'HandBag'}
+        ],
+        modals : [
+          {title:'Jeans',desc:'Jeans are a type of pants or trousers, typically made from denim or dungaree cloth. Often the term "jeans" refers to a particular style of trousers, called "blue jeans", which were invented by Jacob W. Davis in partnership with Levi Strauss & Co. ... Jean cloth can be entirely cotton as well, similar to denim.Here are some adjectives for jeans: baggy, pleated, tough, protective, mangled blue, defiant appeal, newest, blackest, homespun and blue, cut-off blue, long, well-worn, dear ungrateful, tattered, unwashed, dismissal discomfited, tightest white, offensive blue, ersatz twentieth-century, extremely baggy, bloody, dusty, ...'}
+        ],
+        modals2 : [ {title:'Shoes',desc:'A shoe is an item of footwear intended to protect and comfort the human foot. Shoes are also used as an item of decoration and fashion. The design of shoes has varied enormously through time and from culture to culture, with appearance originally being tied to function.Price for these shoes is 90$'}],
+        modals3 : [ {title:'Shirt',desc:'A shirt is a cloth garment for the upper body (from the neck to the waist). Originally an undergarment worn exclusively by men, it has become, in American English, a catch-all term for a broad variety of upper-body garments and undergarments. ... A shirt can also be worn with a necktie under the shirt collar.'}],
+        modals4 : [ {title:'HandBag',desc:'A bag is a kind of soft container. It can hold or carry things. It may be made from cloth, leather, plastic, or paper. Many bags are disposable but some are made to use for a long time. A bag may have one or two handles or a shoulder strap. Bags come in different shapes and sizes depending on how they will be used. A small bag that can be carried with a single hand is sometimes called a handbag, purse, or pocketbook. Children can suffocate, or stop breathing and die, when they put certain kinds of bags over their heads, due to having the supply of oxygen cut off.'}]
+      };  
+    }
+        
+    
+    
+    
+})
